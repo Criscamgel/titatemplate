bc1c985241c22afd50a5c676a0b991213bb7319d
Criscamgel <caguio@ucompensar.edu.co>
2021-06-21 12:58:24 -0500

    Menú finalizado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.scss';
+import logo from '../../images/logo.png';
 
 export const Navbar = () => {
+
+    const items = [
+                    {key: 1, value:'All'},
+                    {key: 2, value:'Branding'}, 
+                    {key: 3, value:'Web'}, 
+                    {key: 4, value:'Photography'}, 
+                    {key: 5, value:'app'}]
+
     return (
-        <div>
-            <h1>Nabvar</h1>
-        </div>
+        <>
+            <div className="contenedor">
+                <div className="logo">
+                    <img src={ logo } alt="Logo" />
+                </div>
+                <div className="menu">
+                        <ul>
+                            { items.map( item => (
+                                <li
+                                  className={`item-${item.key}`}
+                                  key={item.key}
+                                ><p>{item.value}</p></li>
+                            )) }
+                            <li className="search">
+                                <i className="fa fa-search"/>
+                                <input type="text" />
+                            </li>
+                        </ul>
+                </div>
+            </div>
+        </>
     )
 }
